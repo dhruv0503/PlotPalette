6ac6d87acbe7de09b76ded73900abfe67ad84f3d
Bhavya Kashmira <103055855+bhavyakashmira@users.noreply.github.com>
2024-01-17 13:00:08 +0530

    signin Page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,18 @@
+const { blackA, green, mauve, violet } = require('@radix-ui/colors');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...blackA,
+        ...green,
+        ...mauve,
+        ...violet,
+      },
+    },
   },
   plugins: [],
 }
