115ecf3ab181d27fc35137135dc973d451476678
Bhavya Kashmira <103055855+bhavyakashmira@users.noreply.github.com>
2024-03-13 20:14:18 +0530

    logo added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,6 +3,7 @@ import * as HoverCard from '@radix-ui/react-hover-card';
 import { FaInstagramSquare, FaGithub, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6"
 import { BiLogoGmail } from "react-icons/bi";
+import * as Avatar from '@radix-ui/react-avatar';
 function Footer() {
   return (
     <div className=' bg-custom-50 p-10 '>
@@ -12,15 +13,11 @@ function Footer() {
         <div className=' text-custom-10 gap-4 flex m-2'>
           <HoverCard.Root>
             <HoverCard.Trigger asChild>
-              <a
-                className="inline-block cursor-pointer rounded-full shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] outline-none focus:shadow-[0_0_0_2px_white]"
-              >
-                <img
-                  className="block h-[45px] w-[45px] rounded-full"
-                  src="https://pbs.twimg.com/profile_images/1337055608613253126/r_eiMp2H_400x400.png"
-                  alt="Radix UI"
-                />
-              </a>
+              <Avatar.Root className="bg-blackA1 inline-flex h-[50px] w-[50px] select-none items-center justify-center overflow-hidden rounded-full align-middle border border-black ">
+                <Avatar.Fallback className="text-custom-20 bg-custom-30 leading-1 flex h-full w-full items-center justify-center text-[15px] font-bold ">
+                  BK
+                </Avatar.Fallback>
+              </Avatar.Root>
             </HoverCard.Trigger>
             <HoverCard.Portal>
               <HoverCard.Content
@@ -49,15 +46,13 @@ function Footer() {
           </HoverCard.Root>
           <HoverCard.Root>
             <HoverCard.Trigger asChild>
-              <a
-                className="inline-block cursor-pointer rounded-full shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] outline-none focus:shadow-[0_0_0_2px_white]"
-              >
-                <img
-                  className="block h-[45px] w-[45px] rounded-full"
-                  src="https://pbs.twimg.com/profile_images/1337055608613253126/r_eiMp2H_400x400.png"
-                  alt="Radix UI"
-                />
-              </a>
+            
+                <Avatar.Root className="bg-blackA1 inline-flex h-[50px] w-[50px] select-none items-center justify-center overflow-hidden rounded-full align-middle border border-black ">
+                  <Avatar.Fallback className="text-custom-20 bg-custom-30 leading-1 flex h-full w-full items-center justify-center text-[15px] font-bold ">
+                    DA
+                  </Avatar.Fallback>
+                </Avatar.Root>
+          
             </HoverCard.Trigger>
             <HoverCard.Portal>
               <HoverCard.Content
@@ -92,15 +87,11 @@ function Footer() {
           
           <HoverCard.Root>
             <HoverCard.Trigger asChild>
-              <a
-                className="inline-block cursor-pointer rounded-full shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] outline-none focus:shadow-[0_0_0_2px_white]"
-              >
-                <img
-                  className="block h-[45px] w-[45px] rounded-full"
-                  src="https://pbs.twimg.com/profile_images/1337055608613253126/r_eiMp2H_400x400.png"
-                  alt="Radix UI"
-                />
-              </a>
+              <Avatar.Root className="bg-blackA1 inline-flex h-[50px] w-[50px] select-none items-center justify-center overflow-hidden rounded-full align-middle border border-black ">
+                <Avatar.Fallback className="text-custom-20 bg-custom-30 leading-1 flex h-full w-full items-center justify-center text-[15px] font-bold ">
+                  AS
+                </Avatar.Fallback>
+              </Avatar.Root>
             </HoverCard.Trigger>
             <HoverCard.Portal>
               <HoverCard.Content
