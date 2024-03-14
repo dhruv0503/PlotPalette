bd8ab41090b2268179a4fd1968b36be31f9d69fb
Dhruv Agrawal <dhruv2505.dag@gmail.com>
2024-03-14 20:56:28 +0530

    DEPLOYING P9

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import "./firebaseconfig/firebaseConfig"
 import App from './App';
 import { Theme } from '@radix-ui/themes';
 
