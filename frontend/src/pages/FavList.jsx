4bc3a745db819f702422797f611de3547c834e86
Dhruv Agrawal <dhruv2505.dag@gmail.com>
2024-03-23 14:39:54 +0530

    Some Design Changes

--- frontend/src/pages/FavList.jsx
+++ frontend/src/pages/FavList.jsx
@@ -1,10 +1,13 @@
-import React ,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Navbar from './Navbar'
 import { useApi } from '../Context/Contxt'
-import { Link , useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { Text } from '@radix-ui/themes'
+import { useNavigate } from 'react-router-dom'
 function FavList() {
-    const { userData ,upcomingMovies } = useApi();
+    const navigate = useNavigate();
+    const { userData, upcomingMovies } = useApi();
     const { collectionid } = useParams();
     const [userFav, setuserFav] = useState([]);
     const handlefavlist = async () => {
@@ -12,7 +15,7 @@ function FavList() {
             //change userDATAID
             const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}api/users/options?parameter=${collectionid}&userId=${userData.id}`)
             setuserFav(response.data)
-           
+
         } catch (error) {
             console.error('Error fetching movies:', error.message);
         }
@@ -21,27 +24,34 @@ function FavList() {
     useEffect(() => {
         handlefavlist();
     }, [])
-    
+
     return (
         < div className='bg-custom-30 h-screen' >
             <div className='bg-custom-50' ><Navbar /></div>
-            <div className='bg-custom-30  p-3' >
-               <div className=" flex flex-col justify-center items-center  sm:grid sm:grid-cols-3 md:grid-cols-5 sm:justify-around ">
-                    {userFav?.map((movie, index) => (
-                        <div className="max-w-md min-h-sm p-4 overflow-hidden rounded-lg shadow-s m-2 relative">
-                            <div className="relative overflow-hidden w-sm h-sm">
-                                <img className="p-1 w-full h-full object-cover rounded-t-lg border-2 transition duration-300 transform hover:scale-125 " src={`https://image.tmdb.org/t/p/original${movie.poster}`} alt={`${movie.title} backdrop_path`} />
-                                <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 hover:opacity-95 m-5 lg:m-20 transition duration-300">
-                                    <div className='mt-4 '>
-                                        <Link to={`/movies/${movie.tmdbId}`} className='block bg-custom-50 border border-black p-2 rounded-b-lg text-center text-white hover:bg-custom-30 transition duration-300'>
-                                            View Details
-                                        </Link>
+            <div className='flex justify-center items-center h-full'>
+                <div className='bg-custom-30  p-3 ' >
+                    <div className=" flex flex-col justify-center items-center sm:grid sm:grid-cols-3 md:grid-cols-5 sm:justify-around ">
+                        {userFav.length === 0 ?
+                            <div className='flex flex-col items-center justify-center'>
+                                <Text className='text-custom-20 flex justify-center m-2' size={"7"} >Wow.. So Empty</Text>
+                                <button className='bg-custom-20 rounded-lg text-custom-40 flex justify-center p-3' onClick={() => navigate(`/movies/toprated`)}>Look at Top Rated Movies</button>
+                            </div> :
+                            (userFav?.map((movie, index) => (
+                                <div className="max-w-md min-h-sm p-4 overflow-hidden rounded-lg shadow-s m-2 relative">
+                                    <div className="relative overflow-hidden w-sm h-sm">
+                                        <img className="p-1 w-full h-full object-cover rounded-t-lg border-2 transition duration-300 transform hover:scale-125 " src={`https://image.tmdb.org/t/p/original${movie.poster}`} alt={`${movie.title} backdrop_path`} />
+                                        <div className="absolute inset-0 flex flex-col justify-center items-center opacity-0 hover:opacity-95 m-5 lg:m-20 transition duration-300">
+                                            <div className='mt-4 '>
+                                                <Link to={`/movies/${movie.tmdbId}`} className='block bg-custom-50 border border-black p-2 rounded-b-lg text-center text-white hover:bg-custom-30 transition duration-300'>
+                                                    View Details
+                                                </Link>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>               
+                            )))}
+                    </div>
+                </div>
             </div>
         </div>
     )
--- frontend/src/pages/FriendPage.jsx
+++ frontend/src/pages/FriendPage.jsx
@@ -4,8 +4,10 @@ import axios from 'axios';
 import { Text } from '@radix-ui/themes';
 import { CheckIcon, Cross2Icon } from '@radix-ui/react-icons';
 import { useApi } from '../Context/Contxt';
+import { useNavigate } from 'react-router-dom';
 
 function FriendPage() {
+  const navigate = useNavigate();
   const { userData } = useApi();
   const handleAccept = async ({ id }) => {
     try {
@@ -21,8 +23,8 @@ function FriendPage() {
 
   const handleDeny = async ({ id }) => {
     try {
-      await axios.delete( `${process.env.REACT_APP_BACKEND_URL}api/friend/deny?userId=${id}` );
-   
+      await axios.delete(`${process.env.REACT_APP_BACKEND_URL}api/friend/deny?userId=${id}`);
+
       window.location.reload();
     } catch (error) {
       console.error("Error fetching movies:", error.message);
@@ -31,36 +33,40 @@ function FriendPage() {
   console.log(userData)
 
   return (
-      <div className='bg-custom-30' >
-          <div className='bg-gray-900' ><Navbar /></div>
-      <div>
-        
-        <Text className='text-custom-20 flex justify-center ' size={"6"} >Request LIST</Text>
-        {userData?.requestList?.map((friend , index) => (
-          <div className='bg-custom-20 p-3 m-3 rounded-lg flex  justify-between' >
-            <Text size={"5"} >{friend?.userName}</Text>
-             <div className='flex  ' >
-            <CheckIcon height={32} width={32} className='cursor-pointer'
-              onClick={() => handleAccept({ id: friend.id })}
-            />
-              <Cross2Icon height={32} width={32} className='cursor-pointer'
-              onClick={() => handleDeny({ id: friend.id })} />
+    <div className='bg-custom-30' >
+      <div className='bg-gray-900' ><Navbar /></div>
+      <div className='grid grid-cols-2'>
+        <div>
+          <Text className='text-custom-20 flex justify-center ' size={"7"} >Friend List</Text>
+          {userData?.friendList?.map((friend, index) => (
+            <div className='bg-custom-20 p-3 m-3 rounded-lg flex  justify-between' onClick={() => navigate(`/account/${friend?.userName}`)}>
+              <Text size={"5"} >{friend?.userName}</Text>
             </div>
-          </div>
-        ))}
-
-        <Text className='text-custom-20 flex justify-center ' size={"6"} >Friend LIST</Text>
-        {userData?.friendList?.map((friend, index) => (
-          <div className='bg-custom-20 p-3 m-3 rounded-lg flex  justify-between' >
-            <Text size={"5"} >{friend?.userName}</Text>
-
-            
-          </div>
-        ))}
-
-
+          ))}
         </div>
-      
+
+        <div>
+          <Text className='text-custom-20 flex justify-center ' size={"7"} >Request List</Text>
+          {userData?.requestList.length === 0 ?
+            <div className='text-custom-20 p-3 m-3 rounded-lg flex justify-center' >
+              <Text size={"5"} >No Request Pending</Text>
+            </div>
+            :
+            (userData?.requestList?.map((friend, index) => (
+              <div className='bg-custom-20 p-3 m-3 rounded-lg flex  justify-between' >
+                <Text size={"5"} >{friend?.userName}</Text>
+                <div className='flex  ' >
+                  <CheckIcon height={32} width={32} className='cursor-pointer'
+                    onClick={() => handleAccept({ id: friend.id })}
+                  />
+                  <Cross2Icon height={32} width={32} className='cursor-pointer'
+                    onClick={() => handleDeny({ id: friend.id })} />
+                </div>
+              </div>
+            )))
+          }
+        </div>
+      </div>
     </div>
   )
 }
